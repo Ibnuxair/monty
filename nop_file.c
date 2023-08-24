80595b0b82d20d94826621949073eff08fb318de
hancho6319 <abbaauwalharuna@gmail.com>
2023-08-24 09:48:49 +0000

    my commit

---
+++ nop_file.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *nop_file - This function does nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: Always returns nothing
+ */
+void nop_file(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
